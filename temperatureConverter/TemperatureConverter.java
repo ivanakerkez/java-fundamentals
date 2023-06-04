09e13378cf9f999978ceb77ee352d2e3e49660e5
ivanakerkez <124634157+ivanakerkez@users.noreply.github.com>
2023-06-04 23:36:55 +0200

    Add files via upload

--- temperatureConverter/TemperatureConverter.java
+++ temperatureConverter/TemperatureConverter.java
@@ -1,8 +1,8 @@
 package temperatureConverter;
 
-/* Use scanner to enter the temperature in Celsius as an integer value.
-Convert from Celsius to Fahrenheit,
-and the program should print both in the format "31C = 87.8F" (round Fahrenheit to one decimal place). */
+// Use scanner to enter the temperature in Celsius as an integer value.
+// Convert from Celsius to Fahrenheit,
+// and the program should print both in the format "31C = 87.8F" (round Fahrenheit to one decimal place).
 
 import java.util.Scanner;
 
@@ -19,11 +19,13 @@ public class TemperatureConverter {
 
         System.out.printf("%dC = %.1fF", temperatureC, temperatureF);
 
-        /* Let's break that down:
-        %d is a placeholder for an integer value
-        C is a character which represents the Celsius unit
-        %.1f specifies a floating-point number with one decimal place
-        F is a character which represents the Fahrenheit unit */
+        // Let's break that down:
+        // %d is a placeholder for an integer value
+        // C is a character which represents the Celsius unit
+        // %.1f specifies a floating-point number with one decimal place
+        // F is a character which represents the Fahrenheit unit
+
+        input.close();
 
     }
 }
