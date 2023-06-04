75ae3244684c88e131a45c6d019240f58a0b2e28
ivanakerkez <124634157+ivanakerkez@users.noreply.github.com>
2023-06-04 23:26:54 +0200

    Add files via upload

---
+++ printText/InsertAndPrintText.java
@@ -0,0 +1,31 @@
+package printText;
+
+// Use scanner to enter your first and last name separately, and then print your first and last name combined.
+
+import java.util.Scanner;
+
+public class InsertAndPrintText {
+    public static void main(String[] args) {
+
+        // to insert our own text, we'll need a scanner
+        Scanner input = new Scanner(System.in); // input is the name of our scanner
+
+        /* camel case convention:
+        compound words or phrases are written without spaces or punctuation,
+        and the first letter of each word is capitalized except for the first word */
+
+        System.out.print("Enter your first name: ");
+        String firstName = input.next(); // String is a variable used for storing text
+
+        System.out.print("Enter your last name: ");
+        String lastName = input.next(); // input.next() is used to read the next token of input from the user
+
+        /* we can use the + operator between strings to combine them (known as concatenation)
+        note tha we also need " " to insert a blank space between two strings */
+
+        System.out.println(firstName + " " + lastName);
+
+        // run the program and see how it goes :)
+
+    }
+}
---
+++ printText/PrintText.java
@@ -0,0 +1,15 @@
+package printText;
+
+public class PrintText {
+    public static void main(String[] args) { // the main() method - it's required, so you'll see it in every Java program
+        // every block of code must be within curly braces {} and every statement must end with a semicolon ;
+
+        // to print a line of text to the screen, we can use println() method
+        System.out.println("Hello World"); // note that we must put the text inside the quotation marks ""
+
+        // we could also use print() method, but unlike println(), it doesn't insert a new line at the end of the output
+        System.out.print("Hello");
+        System.out.print("World");
+
+    }
+}
