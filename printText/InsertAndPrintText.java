34d2ff2e7f1618128deb373132f9aa35369e9f19
ivanakerkez <124634157+ivanakerkez@users.noreply.github.com>
2023-06-04 23:36:17 +0200

    Add files via upload

--- printText/InsertAndPrintText.java
+++ printText/InsertAndPrintText.java
@@ -10,9 +10,9 @@ public class InsertAndPrintText {
         // to insert our own text, we'll need a scanner
         Scanner input = new Scanner(System.in); // input is the name of our scanner
 
-        /* camel case convention:
-        compound words or phrases are written without spaces or punctuation,
-        and the first letter of each word is capitalized except for the first word */
+        // camel case convention:
+        // compound words or phrases are written without spaces or punctuation,
+        // and the first letter of each word is capitalized except for the first word
 
         System.out.print("Enter your first name: ");
         String firstName = input.next(); // String is a variable used for storing text
@@ -20,10 +20,12 @@ public class InsertAndPrintText {
         System.out.print("Enter your last name: ");
         String lastName = input.next(); // input.next() is used to read the next token of input from the user
 
-        /* we can use the + operator between strings to combine them (known as concatenation)
-        note tha we also need " " to insert a blank space between two strings */
+        // we can use the + operator between strings to combine them (known as concatenation)
+        // note tha we also need " " to insert a blank space between two strings
 
         System.out.println(firstName + " " + lastName);
+
+        input.close(); // it's a good practice to close the scanner after we're done using it
 
         // run the program and see how it goes :)
 
